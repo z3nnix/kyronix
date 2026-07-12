8067617989e567cd73264358200d6a68995c8223
AnmiTaliDev <anmitali198@gmail.com>
2026-07-12 22:03:42 +0500

    feat(kyrobox): add top

--- user/kyrobox/ps.c
+++ user/kyrobox/ps.c
@@ -14,15 +14,16 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    printf("%5s %5s %c %5s %s\n", "PID", "PPID", 'S', "UID", "COMMAND");
+    printf("%5s %5s %c %5s %8s %s\n", "PID", "PPID", 'S', "UID", "MEM(KB)", "COMMAND");
 
     char line[1024];
     while (fgets(line, sizeof(line), f)) {
         unsigned int pid, ppid, uid;
+        unsigned long mem_kb;
         char state;
         char exe[512] = {0};
-        if (sscanf(line, "%u %u %c %u %511s", &pid, &ppid, &state, &uid, exe) >= 4) {
-            printf("%5u %5u %c %5u %s\n", pid, ppid, state, uid, exe);
+        if (sscanf(line, "%u %u %c %u %lu %511s", &pid, &ppid, &state, &uid, &mem_kb, exe) >= 5) {
+            printf("%5u %5u %c %5u %8lu %s\n", pid, ppid, state, uid, mem_kb, exe);
         }
     }
 
